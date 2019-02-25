eaccec20f48e0698ef4a8ec9469ee480c2e8dba5
LadyRavenFire <kilkust@gmail.com>
2019-02-25 17:31:38 +0500

    fly with animation

---
+++ Test task/Assets/Client/Scripts/Objects/Animation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation : MonoBehaviour
+{
+
+    [SerializeField]
+    Sprite[] _frames;
+
+    [SerializeField]
+    float framesPerSecond = 10f;
+
+    private Image _image;
+
+    private RightLeftMoving _rightLeftMoving;
+
+    void Start()
+    {
+        _rightLeftMoving = gameObject.GetComponent<RightLeftMoving>();
+        _image = gameObject.GetComponent<Image>();
+
+        if (_rightLeftMoving.IsLeft)
+        {
+            //Quaternion rotate = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y+360, gameObject.transform.rotation.z, 0);
+            //transform.localRotation = rotate;  -- non clicked
+           
+        }
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        int index = (int)(Time.time * framesPerSecond) % _frames.Length;
+        _image.sprite = _frames[index];
+    }
+}
---
+++ Test task/Assets/Client/Scripts/Objects/RightLeftMoving.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RightLeftMoving : MonoBehaviour
+{
+    public bool IsLeft;
+
+    void FixedUpdate()
+    {
+        if (IsLeft)
+        {
+            Vector3 position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = position;
+        }
+    }
+}
