1cdaf3781c28ba6c03572ed09877af07e949baaa
LadyRavenFire <kilkust@gmail.com>
2019-02-25 23:43:34 +0500

    code refactoring

--- Test task/Assets/Client/Scripts/Managers/SpawnManager.cs
+++ Test task/Assets/Client/Scripts/Managers/SpawnManager.cs
@@ -30,23 +30,18 @@ public class SpawnManager : MonoBehaviour
             _delay = _random.Next(0, 3);
             int leftRight = _random.Next(0, 2);
             int borderOfScreen = (int)(_cornerCoordinates.ScreenSpaceCorners[2].y * 0.1f); //TODO калибровать значения
+
+            int yCoord = (int)_random.Next(0 + borderOfScreen, (int)_cornerCoordinates.ScreenSpaceCorners[2].y - borderOfScreen);
+            var newObject = Instantiate(_simpleObject, new Vector3(10, 10, 0), Quaternion.identity) as GameObject;
+            newObject.transform.SetParent(_mainCanvas.transform, false);
+
             if (leftRight == 0)
             {              
-                int yCoord = (int)_random.Next(0 + borderOfScreen, (int)_cornerCoordinates.ScreenSpaceCorners[2].y - borderOfScreen);
-                print(yCoord);
-                var newObject = Instantiate(_simpleObject, new Vector3(10, 10, 0), Quaternion.identity) as GameObject;
-
-                newObject.transform.SetParent(_mainCanvas.transform, false);
                 newObject.transform.position = new Vector3((int)_cornerCoordinates.ScreenSpaceCorners[2].x + (_cornerCoordinates.ScreenSpaceCorners[2].x * 0.1f), yCoord, 0);
             }
             else
             {
-                int yCoord = (int)_random.Next(0 + borderOfScreen, (int)_cornerCoordinates.ScreenSpaceCorners[2].y - borderOfScreen); 
-                var newObject = Instantiate(_simpleObject, new Vector3(10, 10, 0), Quaternion.identity) as GameObject;
-
-                newObject.transform.SetParent(_mainCanvas.transform, false);
                 newObject.transform.position = new Vector3(-(_cornerCoordinates.ScreenSpaceCorners[2].x * 0.1f), yCoord, 0);
-
                 RightLeftMoving rightLeftMoving = newObject.GetComponent<RightLeftMoving>();
                 rightLeftMoving.IsLeft = false;                
             }
