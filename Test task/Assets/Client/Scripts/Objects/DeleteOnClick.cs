9e5dcca3c371de8023a2f4403ba33e1a0fb2dfea
LadyRavenFire <kilkust@gmail.com>
2019-02-25 16:39:22 +0500

    Create first object

---
+++ Test task/Assets/Client/Scripts/Objects/DeleteOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DeleteOnClick : MonoBehaviour,  IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Destroy(gameObject);
+    }
+}
